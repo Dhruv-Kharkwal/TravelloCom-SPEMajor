abf9e421aad8290c934b00639dd385bbadf680fb
Dhruv-Kharkwal <dhruvkharkwal@gmail.com>
2023-05-09 15:51:10 +0530

    backend post part implemented and backend completed

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,15 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js"
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controllers/posts.js";
+
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 
 /* Configrations */ 
@@ -42,10 +50,12 @@ const upload = multer({storage});
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
-
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 
 const PORT = process.env.PORT || 6001;
@@ -55,5 +65,9 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* Add data one time */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 })
 .catch((error) => console.log(`${error} did not connect`));
