35db18f844753ef0c41dcafae214651e4102733b
Dhruv-Kharkwal <dhruvkharkwal@gmail.com>
2023-05-15 00:02:26 +0530

    new data added

--- frontend/src/scenes/widgets/AdvertWidget.jsx
+++ frontend/src/scenes/widgets/AdvertWidget.jsx
@@ -14,15 +14,16 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/info4.jpeg"
+        src="./info4.jpeg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-      <Typography color={main}>Quote</Typography>
+        <Typography color={main}>Quote</Typography>
         <Typography color={medium}>Here is the quote for day!!!</Typography>
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
-      “Yesterday is history, tomorrow is a mystery, but today is a gift. That is why it is called the present.” – Master Oogway
+        “Yesterday is history, tomorrow is a mystery, but today is a gift. That
+        is why it is called the present.” – Master Oogway
       </Typography>
     </WidgetWrapper>
   );
